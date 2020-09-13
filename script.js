78f46c06b6e78ccd2cb3accd13da34c0687b58bf
Gas-prod <62751705+Gas-prod@users.noreply.github.com>
2020-09-13 16:04:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+var notificationActive;
+var notificationSupporte;
+var nbrNotif = 0;
+
+function horloge() {
+    var date = new Date(); 
+    var heure = String(date.getHours());
+    var minute = String(date.getMinutes());
+
+    if(Number(heure) < 10){
+        heure = "0" + heure;
+    }
+    if(Number(minute) < 10){
+        minute = "0" + minute;
+    }
+
+    document.getElementById("heure").innerHTML = heure + " H " + minute;
+    
+    if(heure == minute){
+        if (document.getElementById("cercle").style.backgroundColor=="rgb(209, 209, 209)"){ 
+            document.getElementById("cercle").style.backgroundColor="black"; 
+            document.getElementById("heure").style.color="white";
+        }else{ 
+            document.getElementById("cercle").style.backgroundColor="rgb(209, 209, 209)"; 
+            document.getElementById("heure").style.color="black";
+        }
+
+        if(document.getElementById("checkbox").checked == true && notificationActive == true && nbrNotif == 0){
+            console.log("notif");
+            showNotif("Il est " + heure + " H " + minute + ", touchez votre nez et du rouge");
+            nbrNotif ++;
+        }
+    }else{
+        document.getElementById("cercle").style.backgroundColor="rgb(209, 209, 209)";
+        document.getElementById("heure").style.color="black";
+        nbrNotif = 0;
+    }
+
+    if(document.getElementById("checkbox").checked == true && notificationSupporte == false){
+        alert("Votre naviguateur ne supporte pas les notifications");
+        document.getElementById("checkbox").checked = false;
+    }
+    if(document.getElementById("checkbox").checked == true && notificationActive == false){
+        alert("Vous n'avez pas accepté les notifications");
+        document.getElementById("checkbox").checked = false;
+    }
+}
+setInterval("horloge()", 1000);
+
+notifPermission();
+
+function notifPermission(){
+    if(!("Notification" in window)){
+        notificationSupporte = false;
+        notificationActive = false;
+    }else if(Notification.permission === "granted"){
+        notificationSupporte = true;
+        notificationActive = true;
+        console.log("notif permise");
+    }else if(Notification.permission !== "denied"){
+        Notification.requestPermission().then(permission => {
+            if(permission === "granted"){
+                notificationSupporte = true;
+                notificationActive = true;
+                console.log("notif permise");
+            }else{
+                notificationSupporte = true;
+                notificationActive = false;
+                console.log("notif pas permise");
+            }
+        });
+    }
+}
+
+function showNotif(notif){
+    const notification = new Notification(notif);
+}
